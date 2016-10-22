5b17b277f445fcf2ccbf416ca41abd382d3c79f6
Ermogenes Palacio <ermogenes@gmail.com>
2016-10-22 12:20:13 -0200

    Corrige comentários do exercício 2, semana 3

--- semana3/ex02.asm
+++ semana3/ex02.asm
@@ -1,8 +1,11 @@
-# MPIS - Exemplo aula 03 Organizacao Computadores
-# da Univesp.
+# MIPS - Exemplo da aula 03 de 
+# Organizacao de Computadores da Univesp.
+#
 # Configurar MARS 4.5:
+#
 # Settings, Memory Configuration,
 # Compact, Data at Address 0
+#
 lw	$v1,	0($a0)
 addi	$v0,	$v0,	1
 sw	$v1,	0($a1)
